26d2d91e807603bb8886f1326110d8cf162e6333
tanisshka <tanishkapatil0102@gmail.com>
2025-11-06 23:21:41 +0530

    Added first React project - 01-react-basics

---
+++ 01-react-basics/src/App.jsx
@@ -0,0 +1,24 @@
+export default function App() {
+  return (
+    <main className="app">
+      <div className="card">
+        <div className="tea">
+          <div className="cup">
+            <div className="steam">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+
+        <h1 className="title"> React</h1>
+        <p className="subtitle">My first cozy React project ☕️</p>
+        <div className="btn-row">
+          <button className="cta">Start learning</button>
+          <button className="ghost">Explore</button>
+        </div>
+      </div>
+    </main>
+  );
+}
